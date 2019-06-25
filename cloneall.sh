8012b8246d118c7259daf2bfff3ea46ef736f00d
adrijshikhar <adrijshikhar85@gmail.com>
2019-06-25 20:26:53 +0530

    fixes

--- cloneall.sh
+++ cloneall.sh
@@ -6,7 +6,7 @@ token="" #enter your user token from github
 organization="" #enter organization name
 
 
-repo_list=$(curl https://api.github.com/orgs/$organization/repos?type=private\&per_page=200 -u ${user}:${token}  | jq .[].ssh_url | sed -e 's/^"//'  -e 's/"$//')
+repo_list=$(curl https://api.github.com/orgs/$organization/repos?type=private\&per_page=1000 -u ${user}:${token}  | jq .[].ssh_url | sed -e 's/^"//'  -e 's/"$//')
 
 for repo in $repo_list
 do
